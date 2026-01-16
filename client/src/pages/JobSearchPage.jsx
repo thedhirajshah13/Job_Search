6707c7160014041ec7f0925a4df666177486617f
Dhiraj Shah <shahdhiraj0002@gmail.com>
2026-01-16 15:42:50 +0530

    change the color of the button

--- client/src/pages/JobSearchPage.jsx
+++ client/src/pages/JobSearchPage.jsx
@@ -126,6 +126,7 @@ const JobSearchPage = () => {
                   minWidth: "110px",
                   height: "30px",
                   marginLeft: "10px",
+                  color:"white"
                 }}
               >
                 Find jobs
